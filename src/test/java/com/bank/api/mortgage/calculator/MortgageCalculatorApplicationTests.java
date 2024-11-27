3d05798f6b30fd400ddc838a59d55eede0929df0
Karthikeyan Subramanian <karthick.careers@gmail.com>
2024-11-27 21:29:58 +0100

    Application initial setup

---
+++ src/test/java/com/bank/api/mortgage/calculator/MortgageCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.api.mortgage.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
